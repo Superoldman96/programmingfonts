7e3b04396209020c6def641df422d4a47c108c35
Daniel Arnold <darnold8@gmail.com>
2025-04-13 14:27:46 -0400

    add Ruby code sample

--- modules/samples.js
+++ modules/samples.js
@@ -22,6 +22,9 @@ export class Samples {
       case 'python':
         code = Py;
         break;
+      case 'ruby':
+        code = Ruby;
+        break;
       case 'shell':
         code = Bash;
         break;
@@ -388,4 +391,68 @@ All mimsy were the borogoves,
 	And the mome raths outgrabe.
 
 - "Jabberwocky" by Lewis Carroll
+`
+
+const Ruby =
+`# frozen_string_literal: true
+
+class OrdersController < ApplicationController
+  before_action :set_order, except: %i[index new create]
+
+  NUMERALS = 1234567890
+  SIMILAR = 'oO08 iIlL1 g9qCGQ'
+  DIACRITICS_ETC = 'â é ù ï ø ç Ã Ē Æ œ'
+
+  def index
+    @orders = Order.all
+  end
+
+  def new
+    @order = Order.new
+  end
+
+  def show; end
+
+  def create
+    @order = Order.new(order_params)
+
+    if @order.save
+      flash[:notice] = 'Order was successfully created.'
+      redirect_to @order
+    else
+      flash[:alert] = @order.errors.full_messages
+      render action: 'new'
+    end
+  end
+
+  def update
+    if @order.update(order_params)
+      flash[:notice] = 'Order was successfully updated.'
+      redirect_to @order
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    @order.destroy
+
+    redirect_to orders_url
+  end
+
+  private
+
+  def set_order
+    @order = Order.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'Order not found'
+    redirect_to orders_path
+  end
+
+  def order_params
+    params.require(:order).permit(:name, :details)
+  end
+end
+
+# adapted from https://github.com/FastArrow1019/RubyOnRails/
 `
