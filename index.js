eef9ba8903af5300c0e1dc97bdbd98dd30e438b7
Koen Lageveen <braver@users.noreply.github.com>
2026-06-13 22:04:32 +0200

    add counter and visual flourish on group toggle

--- index.js
+++ index.js
@@ -121,12 +121,10 @@ function selectFont () {
 }
 
 const chevronDownIcon = '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M12.78 5.22a.749.749 0 0 1 0 1.06l-4.25 4.25a.749.749 0 0 1-1.06 0L3.22 6.28a.749.749 0 1 1 1.06-1.06L8 8.939l3.72-3.719a.749.749 0 0 1 1.06 0Z"/></svg>'
-const chevronUpIcon = '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="currentColor"><path d="M3.22 10.53a.749.749 0 0 1 0-1.06l4.25-4.25a.749.749 0 0 1 1.06 0l4.25 4.25a.749.749 0 1 1-1.06 1.06L8 6.811 4.28 10.53a.749.749 0 0 1-1.06 0Z"/></svg>'
 
 window.toggleGroup = (alias) => {
   const primary = document.querySelector(`#select-font [data-alias='${alias}']`)
   const isExpanded = primary.classList.toggle('group-open')
-  primary.querySelector('.group-toggle').innerHTML = isExpanded ? chevronUpIcon : chevronDownIcon
   document.querySelectorAll(`#select-font [data-group='${alias}']`).forEach((child) => {
     child.classList.toggle('group-child-visible', isExpanded)
   })
@@ -205,7 +203,7 @@ function renderSelectList () {
 
       const childList = groups[v.alias] || []
       const chevron = childList.length > 0
-        ? `<button title="Alternatives" class="group-toggle" onclick="toggleGroup('${v.alias}')">${chevronDownIcon}</button>`
+        ? `<button title="Alternatives" class="group-toggle" onclick="toggleGroup('${v.alias}')">+${childList.length} ${chevronDownIcon}</button>`
         : ''
 
       option.innerHTML = `
